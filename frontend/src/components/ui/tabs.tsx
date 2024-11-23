d776eb28480f12d8489b1226597628070633c0a0
programmerraja <programmerraja@users.noreply.github.com>
2024-11-23 15:37:19 +0530

    css fix

--- frontend/src/components/ui/tabs.tsx
+++ frontend/src/components/ui/tabs.tsx
@@ -3,7 +3,8 @@
 import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { cn } from "../../lib/utils";
+import { cn } from "../../utils/cssutils";
+
 
 const Tabs = TabsPrimitive.Root;
 
